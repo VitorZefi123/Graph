279f84dea6d27721f1b652f31b9a1ca2a8ee151f
VitorZefi123 <vitorzefi@smartsearchai514.onmicrosoft.com>
2025-03-14 13:06:28 +0100

    Minor infrastructure improvments

--- GraphDevelopment/GraphProject/Server/backend_project/PythonServer/app.py
+++ GraphDevelopment/GraphProject/Server/backend_project/PythonServer/app.py
@@ -1,15 +1,26 @@
 from flask import Flask, jsonify
-from pymilvus import connections
+from pymilvus import connections,utility
 
 app = Flask(__name__)
 
+# Simple test connection to Milvus
 @app.route('/test_connection', methods=['GET'])
 def test_connection():
     try:
-        connections.connect("default", host="127.0.0.1", port="19530")
-        
-        return jsonify({"message": "Successfully connected to Milvus!"}), 200
+        # Connect to Milvus server using the provided URI
+        uri = "http://192.168.100.164:19530"
+        connections.connect(uri=uri)
+
+        # List all collections in the Milvus database
+        collection_names = utility.list_collections()
+
+        # If the connection is successful and collections are fetched, return the collections
+        return jsonify({
+            "message": "Successfully connected to Milvus!",
+            "collections": collection_names
+        }), 200
     except Exception as e:
+        # In case of failure, return an error message
         return jsonify({"message": f"Failed to connect to Milvus: {str(e)}"}), 500
 
 # A simple default route
